ded2f6f2f3188320fb3824b2edee699637616ac0
David Charles <charled1@gmatc.matc.edu>
2022-10-02 14:55:25 -0500

    added score validation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';  // node imports this from node_modules
 import connectDatabase from './config/db';
+import {check, validationResult } from 'express-validator';
 
 
 const app = express();  //creating express object named app
@@ -23,9 +24,20 @@ app.get('/', (req, res) =>
  * @route Post api/highScores
  * @desk Post highScore
  */
-app.post('/api/highScores', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+app.post('/api/highScores', 
+[
+    check('initials', 'Please enter your initials.').not().isEmpty(),
+    check('score', 'Error: NULL score value!').not().isEmpty(),
+    check('score', 'Error: Non Integer value.').isInt()
+],(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(422).json({errors: errors.array() });
+    }else{
+        return res.send(req.body);
+    }
+/*     console.log(req.body);
+    res.send(req.body); */
 });
 
 //Connection Listener
